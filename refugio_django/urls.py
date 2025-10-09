fc67e211a98fe3af3842ba05c8373a00e814237c
Alex-Go1 <137842113+Alex-Go1@users.noreply.github.com>
2025-10-08 23:36:09 -0600

    pagina principal lista

---
+++ refugio_django/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Rutas de tus apps
+    path('', include('core.urls')),
+    path('usuarios/', include('usuarios.urls')),
+    path('animales/', include('animales.urls')),
+    path('adopciones/', include('adopciones.urls')),
+    path('productos/', include('productos.urls')),
+    path('servicios/', include('servicios.urls')),
+    path('avisos/', include('avisos.urls')),
+    path('', include('core.urls')),
+   # path('dashboard/', include('dashboard.urls')),
+]
